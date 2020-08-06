2f95868d2aa7914f36ecdd5677cee3bed86308a5
Adam <weterby2@gmail.com>
2020-08-06 16:56:03 +0200

    Rebuild game state setup

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '');
-
-game.state.add('play', {
-    preload: function() {
-
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 200 }
+        }
     },
-    create: function() {
-
-    },
-    render: function() {
-
+    scene: {
+        preload: preload,
+        create: create,
     }
-});
+};
 
-game.state.start('play');
+let game = new Phaser.Game(config);
+
+function preload ()
+{
+    //backgrounds
+    this.load.image('background1', 'src/img/background1.png');
+    //specials
+    this.load.image('coin', 'src/img/coin.png');
+}
+
+function create ()
+{
+    let background1 = this.add.image(400, 300, 'background1');
+   // let coin = this.add.image(400, 300, 'coin');
+}
